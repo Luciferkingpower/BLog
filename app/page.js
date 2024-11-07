27ec7d90396d10ae3ff0a42fae220e0b5c2e6bf6
Luciferkingpower <111267988+Luciferkingpower@users.noreply.github.com>
2024-11-07 18:25:20 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import { useRouter } from 'next/router';
 
 export default function Home() {
 
-  {const router = useRouter();
+  const router = useRouter();
 
   const navigateToContact = () => {
     router.push('/contact');
